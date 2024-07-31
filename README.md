9f8b68ca2cbd9e9bc8990fabebcab1e968d71bab
MioDevelops <94136291+MioDevelops@users.noreply.github.com>
2024-07-30 22:15:01 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# ✨🌙 Your Celestial Gaurdian, Selene! 🌙✨
+Selene is a discord bot that I have created to use for my personal server, to enhance my members' experience and make my server a haven for friendships to blossom.
+<br /><br />
+
+# 🌑 Selene's Commands 🌑
+## 1. `/help`
+This command provides the user will all of Selene's commands, and how they can be used.
+<br /><br />
+
+## 2. `/create`
+### `/create ticket`: Lets members report other users that violate the rules of my server.
+### `/create report`: Lets members create a feedback / bug report.
+<br /><br />
+
+## 3. `/set`
+### `/set creator`: This allows my Cosmic Creators to set their personal creator link (link to their media's) so Selene can alert the Celestial Cartographers.
+<br /><br />
+
+# 🌟 Contributions 🌟
+Since Selene is open source, I am very limited on how you can contribute. If you'd like, you may fork and make a pull request and add new features or enhancements that you see fit. Your input is always welcome !!
+<br /><br />
+
+# 🌌 Contact 🌌
+If you have any questions, or you see any ways a user can abuse my bot, you can reach out to me directly through discord or email.
+### Email: lunardemimoon@gmail.com
+### Discord: lunardemimoon
---
+++ src/out_source/LunarTools.js
@@ -0,0 +1,55 @@
+const { Client } = require("discord.js");
+const req = require("request");
+
+class LunarTools
+{
+    /**
+     * @param {Client} client 
+     */
+    constructor(client)
+    {
+        this.client = client;
+        this.LastCommit = "";
+
+        this.#RetrieveLastCommit();
+    }
+
+    // Private Void; Retrieve the last commit message via GIT API \\
+    #RetrieveLastCommit() {
+        req({
+            url: 'https://api.github.com/repos/MioDevelops/Selene-DB/commits',
+            headers: {
+                'Accept': 'application/vnd.github+json',
+                'Authorization': `Bearer ${process.env.GITAUTH}`,
+                'X-GitHub-Api-Version': '2022-11-28',
+                'user-agent': 'node.js'
+            }
+        }, (err, res, body) => {
+            if(!err && res.statusCode == 200)
+                // TODO: Check last commit time
+                switch(JSON.parse(body)[0].commit.message)
+                {
+                    case "test":
+                        this.LastCommit = "Selene is currently in debugging mode, all logs will show below."; return;
+                    case "NoAlert":
+                        this.LastCommit = "NoAlert"; return;
+                    default: this.LastCommit = JSON.parse(body)[0].commit.message; return;
+                }
+        });
+    }
+
+    /**
+     * Sends a message to the Alerts channel of the Lunar Eclipse.
+     * 
+     * @param {string} message
+     * 
+     */
+    SendDebugMessage(message)
+    {
+        if(!message) return;
+        
+        this.client.channels.cache.get("1236893338758811742").send(message)
+    }
+}
+
+module.exports = { LunarTools };
